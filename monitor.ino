38d4c50e2ba821a09e049e81aba0b6eb18c5ef90
Brian Luft <brian@unbracketed.com>
2012-09-19 20:21:31 -0700

    basic Serial sync

--- monitor.ino
+++ monitor.ino
@@ -1,7 +1,7 @@
 /*
 dev-lights
-
-Ambient feedback indicators for CI or test suite-related events
+ 
+ Ambient feedback indicators for CI or test suite-related events
  */
 
 int debug_led = 8;
@@ -12,6 +12,7 @@ int indicator_B = 11;
 
 void setup() {                
   pinMode(debug_led, OUTPUT);   
+  digitalWrite(debug_led, HIGH);
   pinMode(indicator_R, OUTPUT);   
   pinMode(indicator_G, OUTPUT);   
   pinMode(indicator_B, OUTPUT); 
@@ -20,6 +21,9 @@ void setup() {
   blink_channel(indicator_R);
   blink_channel(indicator_G);
   blink_channel(indicator_B);  
+
+  Serial.begin(9600);
+  establishContact();
 }
 
 
@@ -32,6 +36,21 @@ void blink_channel(int pin) {
 
 
 void loop() {
-  blink_channel(debug_led);
+  //  blink_channel(debug_led);
+  //   if we get a valid byte, read analog ins:
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    int inByte = Serial.read();
+    blink_channel(indicator_G);
+  }
 }
+
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.print('A');   // send a capital A
+    delay(300);
+  }
+}
+
+
 
